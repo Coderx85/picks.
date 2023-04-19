64deca85d5087bc00998595d121fdfd9d514ea62
Utkarsh <hauntedutkarsh@gmail.com>
2023-04-19 19:01:55 +0530

    modified header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,17 @@ import React from "react";
 import logo5 from "../assets/logo5.png";
 import { Link } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
+import { RiCustomerService2Fill } from "react-icons/ri";
 
 const Header = () => {
   return (
     <header>
-        {/* Top Nav */}
+      {/* Top Nav */}
 
-      <div className="flex items-center bg-black flex-grow justify-between">
+      <div className="flex font-semibold items-center bg-black flex-grow justify-between">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Link to={""}>
-            <div className="h-12 ">
+            <div className="h-12 mx-2">
               <img src={logo5} className=" h-10 object-contain " />
             </div>
           </Link>
@@ -19,34 +20,36 @@ const Header = () => {
           {/* Search Bar */}
 
           <div className="">
-            <CiSearch className="h-12 p-4 color:white"/>
+            <CiSearch className="" />
           </div>
         </div>
 
         {/* Right Nav */}
 
-        <div className="">
+        <div className="flex items-center text-xs space-x-4 mx-3 whitespace-nowrap ">
           <Link
-            className="px-4 py-1 text-sm rounded-full border bg-transparent hover:bg-white hover:bg-opacity-25"
+            className="cursor-pointer flex items-center px-4 py-1 text-sm rounded-full border bg-transparent hover:bg-white hover:bg-opacity-25"
             to={""}
           >
-            Help {"&"} Contact
+            <p>Help {"&"} Contact .</p>
+            <RiCustomerService2Fill className="h-5" />
           </Link>
+
           <Link
-            className="px-4 py-1 text-sm rounded-full border bg-transparent hover:bg-white hover:bg-opacity-25"
+            className="cursor-pointer px-4 py-1 text-sm rounded-full border bg-transparent hover:bg-white hover:bg-opacity-25 "
             to={""}
           >
-            Log In
+            <p>Log In</p>
           </Link>
+
           <Link
-            className="px-4 py-1 text-sm rounded-full border bg-[#d4b962] text-black hover:bg-white hover:bg-opacity-25 border-transparent bg-transparent hover:bg-[#dfcb89;]"
+            className="cursor-pointer px-4 py-1 text-sm rounded-full border bg-[#d4b962] text-black hover:bg-white hover:bg-opacity-25 border-transparent bg-transparent hover:bg-[#dfcb89;]"
             to={""}
           >
-            Register
+            <p>Register</p>
           </Link>
         </div>
       </div>
-
     </header>
   );
 };
