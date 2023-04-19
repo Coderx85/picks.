b51690083e41fcc61e82574410ae2af1a31d3602
Utkarsh <hauntedutkarsh@gmail.com>
2023-04-19 10:53:01 +0530

    modified assets

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
-import logo2 from "../assets/logo2.png";
+import logo5 from "../assets/logo5.png";
 
 const Header = () => {
   return (
     <header>
       {/* desktop */}
 
-      <div className="fixed shadow-md w-full h-18 px-2 md:px-4">
-        <div className="h-16">
-          <img src={logo2} className="h-full" />
+      <div className="bg-black fixed shadow-md w-full h-14 px-2 md:px-4">
+        <div className="h-10">
+          <img src={logo5} className="h-full" />
         </div>
       </div>
       {/* mobile */}
