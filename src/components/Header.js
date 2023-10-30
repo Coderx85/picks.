dd71ccb0c8691896e6809af3ffa4a6d53863969a
Shriyam Gera <shriyamgera@gmail.com>
2023-10-31 01:30:09 +0530

    Added Search Bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log('Search term:', searchTerm);
+    window.location.href = `/search?q=${searchTerm}`;
+  };
+
   return (
     <header>
       <div
-        className="max-w-[1440px] flex flex-grow justify-between items-centre p-2 bg-neutral-400	
-      "
+        className="max-w-[1440px] flex flex-grow justify-between items-center p-2 bg-neutral-400"
       >
         {/* Left Nav */}
 
-        <div className="font-face-gm flex items-center text-black text-3xl cursor-pointer px-[24px] py-[8px] font-extrabold">
-          picks <span className=" text-green-700">.</span>
+        <div className="font-face-gm flex items-center text-black text-3xl cursor-pointer px-6 py-2 font-extrabold">
+          picks <span className="text-green-700">.</span>
         </div>
 
         {/* Right Nav */}
 
-        <div className="text-black flex items-center space-x-4 mx-3 whitespace-nowrap ">
-          <Link className="px-2" to={""}>
+        <div className="text-black flex items-center space-x-4 mx-3 whitespace-nowrap">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="border border-black px-2 py-1 rounded"
+          />
+          <button onClick={handleSearch} className="border border-black px-2 py-1 rounded">Search</button>
+          <Link className="px-2" to="/become-a-seller">
             <p>Become a Seller</p>
           </Link>
 
-          <Link className="px-2" to={""}>
-            <p className="">Help {"&"} Contact</p>
+          <Link className="px-2" to="/help-and-contact">
+            <p>Help & Contact</p>
           </Link>
 
-          <Link className="px-2" to={""}>
+          <Link className="px-2" to="/sign-in">
             <p>Sign In</p>
           </Link>
 
-          <Link className="border border-black px-4 p-1 rounded " to={""}>
-            <p className="text-green-700	">Join</p>
+          <Link className="border border-black px-4 p-1 rounded" to="/join">
+            <p className="text-green-700">Join</p>
           </Link>
         </div>
       </div>
